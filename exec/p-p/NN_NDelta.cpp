cbc820d143130fc53550cb5052f79013cfdfa823
mzetenyi <miklos.zetenyi@gmail.com>
2022-05-24 16:59:58 +0200

    add cross section for resonance prod

---
+++ exec/p-p/NN_NDelta.cpp
@@ -0,0 +1,80 @@
+#include "NN_NDelta.hpp"
+
+#include "Config.hpp"
+#include "Vectors.hpp"
+using namespace Vectors;
+#include "FeynmanRules.hpp"
+#include "MultiArray.hpp"
+#include "wavefunc.hpp"
+
+Amplitude_NN_NDelta::Amplitude_NN_NDelta(double srt, int QN, int QD)
+    : srt(srt), QN(QN), QD(QD) {}
+
+Kinema2 Amplitude_NN_NDelta::getInputKinematics() {
+  double mN = Config::get<double>("mN");
+  return Kinema2(srt, mN, mN);
+}
+
+Kinema2 Amplitude_NN_NDelta::getOutputKinematics(double m) {
+  double mN = Config::get<double>("mN");
+  return Kinema2(srt, mN, m);
+}
+
+double Amplitude_NN_NDelta::width(double m) {
+  double Gamma = Config::get<double>("GammaD");
+  return Gamma;
+}
+
+double Amplitude_NN_NDelta::spectralFunction(double m) {
+  double mD = Config::get<double>("mD");
+  double Gamma = width(m);
+  return 1. / pi_ * mD * Gamma /
+         (POW<2>(m * m - mD * mD) + mD * mD * Gamma * Gamma);
+}
+
+double Amplitude_NN_NDelta::MSQR(double m, double costh) {
+  double mpi = Config::get<double>("mpi");
+  Kinema2 kinIn = getInputKinematics();
+  Kinema2 kinOut = getOutputKinematics(m);
+  FourVector p1 = kinIn.p1();
+  FourVector p2 = kinIn.p2();
+  FourVector p3 = kinOut.p1(costh);
+  FourVector p4 = kinOut.p2(costh);
+  FourVector ppi = p1 - p3;
+  int Q1 = 1;
+  int Q2 = 1;
+  int Q3 = QN;
+  int Q4 = QD;
+  int Qpi = Q1 - Q3;
+  ubar_ ubarN(half, p3);
+  ubar_ ubarD(3 * half, p4);
+  u_ u1(half, p1);
+  u_ u2(half, p2);
+  MultiArray<dcomplex> helicityAmplitude(idx_s1h, idx_s1h, idx_s1h, idx_s3h);
+  for (halfint la1 : {-half, half}) {
+    for (halfint la2 : {-half, half}) {
+      for (halfint la3 : {-half, half}) {
+        for (halfint la4 : {-3 * half, -half, half, 3 * half}) {
+          for (int mu : {0, 1, 2, 3}) {
+            helicityAmplitude(la1, la2, la3, la4) +=
+                ubarN(0, la3) * vertexNNpi(QN, -ppi, -Qpi) * u1(0, la1) *
+                scalarPropagator(ppi, mpi) * sign_(mu) * ubarD(mu, la4) *
+                vertexDNpi(QD, ppi, Qpi, mu) * u2(0, la2);
+          }
+        }
+      }
+    }
+  }
+  double MSQR(0);
+  for (halfint la1 : {-half, half}) {
+    for (halfint la2 : {-half, half}) {
+      for (halfint la3 : {-half, half}) {
+        for (halfint la4 : {-3 * half, -half, half, 3 * half}) {
+          dcomplex amp = helicityAmplitude(la1,la2,la3,la4);
+          MSQR += real(amp*conj(amp));
+        }
+      }
+    }
+  }
+  return MSQR;
+}
--- lib/FeynTools/CrossSection.hpp
+++ lib/FeynTools/CrossSection.hpp
@@ -6,9 +6,9 @@
 #include "utils.hpp"
 
 template <typename Amplitude>
-class CrossSection {
+class CrossSectionSS {
  public:
-  CrossSection(Amplitude amp) : amplitude(amp) {}
+  CrossSectionSS(Amplitude amp) : amplitude(amp) {}
   double dsigma_dcosth(double costh) {
     Kinema2 kinIn = amplitude.getInputKinematics();
     Kinema2 kinOut = amplitude.getOutputKinematics();
@@ -20,12 +20,53 @@ class CrossSection {
     int ntheta = 20;
     if (isSet("CrossSection.ntheta"))
       ntheta = Config::get<int>("CrossSection.ntheta");
-    double dtheta = 2./ntheta;
+    double dtheta = 2. / ntheta;
     double sum(0.);
-    for (double theta(-1.+dtheta); theta<1.; theta+=dtheta) {
+    for (double theta(-1. + dtheta); theta < 1.; theta += dtheta) {
       sum += dsigma_dcosth(theta);
     }
-    return sum/n * 2.;
+    return sum / n * 2.;
+  }
+
+ private:
+  Amplitude amplitude;
+};
+
+template <typename Amplitude>
+class CrossSectionSR {
+ public:
+  CrossSectionSR(Amplitude amp) : amplitude(amp) {}
+  double dsigma_dm_dcosth(double m, double costh) {
+    Kinema2 kinIn = amplitude.getInputKinematics();
+    Kinema2 kinOut = amplitude.getOutputKinematics(double m);
+    double srt = kinIn.M;
+    return 1. / (64. * pi_ * pi_ * srt * srt) * kinOut.pabs() / kinIn.pabs() *
+           amplitude.MSQR(m, costh) * 2. * m * amplitude.spectralFunction(m);
+  }
+  double dsigma_dm(double m) {
+    int ncosth = 20;
+    if (isSet("CrossSection.ntheta"))
+      ncosth = Config::get<int>("CrossSection.ncosth");
+    double dcosth = 2. / ntheta;
+    double sum(0.);
+    for (double costh(-1. + dcosth); costh < 1.; costh += dcosth) {
+      sum += dsigma_dm_dcosth(m, costh);
+    }
+    return sum / n * 2.;
+  }
+  double sigmaTot() {
+    Kinema2 kinOut = amplitude.getOutputKinematics(0.);
+    double srt = kinOut.M;
+    double m1 = kinOut.m1;
+    double mmax = srt - m1;
+    int nm = 20;
+    if (isSet("CrossSection.nm")) nm = Config::get<int>("CrossSection.nm");
+    double dm = 2. / nm;
+    double sum(0.);
+    for (double m(dm); m < mmax; m += dm) {
+      sum += dsigma_dm(m);
+    }
+    return sum / n * mmax;
   }
 
  private:
