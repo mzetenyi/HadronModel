fe312e6dbcd838195ef5d094fece89689838429d
mzetenyi <miklos.zetenyi@gmail.com>
2022-05-25 13:39:30 +0200

    errors corrected

--- exec/p-p/FeynmanRules.cpp
+++ exec/p-p/FeynmanRules.cpp
@@ -3,14 +3,14 @@
 
 DiracMatrix vertexNNpi(int Q_Nout, FourVector p_pion, int Q_pion) {
   double f_NNpi = Config::get<double>("f_NNpi");
-  double m_pi = Config::get<double>("m_pi");
+  double m_pi = Config::get<double>("mpi");
   return f_NNpi / m_pi * gamma5_ * gamma_(p_pion) *
          isospin_1h1h1(Q_Nout, Q_Nout - Q_pion, Q_pion);
 }
 
 double vertexDNpi(int Q_Dout, FourVector p_pion, int Q_pion, int mu) {
   double f_DNpi = Config::get<double>("f_DNpi");
-  double m_pi = Config::get<double>("m_pi");
+  double m_pi = Config::get<double>("mpi");
   return f_DNpi / m_pi * p_pion(mu) *
          isospin_3h1h1(Q_Dout, Q_Dout - Q_pion, Q_pion);
 }
