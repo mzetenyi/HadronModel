fa21a5f214ae45d148f2eb6316a7c11cb0bd64e2
mzetenyi <miklos.zetenyi@gmail.com>
2022-05-25 13:41:44 +0200

    added main for NN_ND

---
+++ exec/p-p/NN_NDelta_main.cpp
@@ -0,0 +1,29 @@
+#include "Amplitude_NN_NDelta.hpp"
+#include "CrossSection.hpp"
+#include "units.hpp"
+using namespace units_GeV;
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  Config::load(argc, argv);
+  int QN = getParam<int>("QN", 0);
+  int QD = getParam<int>("QD", 2);
+  cout << "# NN_NDelta: cross section of p+p -> N + Delta" << endl;
+  cout << "\n#parameters:" << endl;
+  Config::list(cout, "# ");
+  cout << "\n# Total cross section" << endl;
+  cout << "#" << setw(9) << "sqtr(s)" << " " << setw(12) << "sigtot" << endl;
+  cout << "#" << setw(9) << "[GeV]" << " " << setw(12) << "[mb]" << endl;
+  double srtmin = getParam<double>("srtmin", 2. * GeV);
+  double srtmax = getParam<double>("srtmax", 5. * GeV);
+  double dsrt = getParam<double>("dsrt", 0.1 * GeV);
+  for (double srt(srtmin); srt < srtmax; srt += dsrt) {
+    Amplitude_NN_NDelta amplitude(srt, QN, QD);
+    CrossSectionSR<Amplitude_NN_NDelta> xsec(amplitude);
+    cout << setw(10) << GeV(srt) << " " << setw(12) << mb(xsec.sigmaTot()) << endl;
+  }
+  return 0;
+}
