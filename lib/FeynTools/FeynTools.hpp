0d9ca394cd1ce38e4ec53422f544437720036fa0
mzetenyi <miklos.zetenyi@gmail.com>
2022-09-21 10:11:54 +0200

    new header including all headers

---
+++ lib/FeynTools/FeynTools.hpp
@@ -0,0 +1,22 @@
+#ifndef FEYNTOOLS_HPP
+#define FEYNTOOLS_HPP
+
+#include "Config.hpp"
+#include "CrossSection.hpp"
+#include "halfint.hpp"
+#include "HelicityAmplitudes.hpp"
+#include "Histogram.hpp"
+#include "Isospin.hpp"
+#include "Kinema.hpp"
+#include "MultiArray.hpp"
+#include "RandomNumberGenerator.hpp"
+#include "require.h"
+#include "Spinors.hpp"
+#include "Tabulator.hpp"
+#include "udouble.hpp"
+#include "units.hpp"
+#include "utils.hpp"
+#include "Vectors.hpp"
+#include "wavefunc.hpp"
+
+#endif // FEYNTOOLS_HPP
