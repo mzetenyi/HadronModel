e0b9470b665b7908087e77d16d06879bb0e444ee
mzetenyi <miklos.zetenyi@gmail.com>
2022-10-20 22:40:24 +0200

    add pp_ND

---
+++ exec/p-p/FeynmanRules.hpp
@@ -0,0 +1,15 @@
+#ifndef FEYNMANRULES_HPP
+#define FEYNMANRULES_HPP
+
+#include "Config.hpp"
+#include "Spinors.hpp"
+using namespace Spinors;
+#include "Vectors.hpp"
+using namespace Vectors;
+
+double formFactor(double t);
+DiracMatrix vertexNNpi(int Q_Nout, FourVector p_pion, int Q_pion);
+double vertexDNpi(int Q_Dout, FourVector p_pion, int Q_pion, int mu);
+dcomplex scalarPropagator(FourVector p, double m);
+
+#endif // FEYNMANRULES_HPP
